f741c33768a7b63902af625982d144120c4f7cbe
Harmeet Bindra <harmeet@harmeets-mbp.attlocal.net>
2017-03-23 00:51:36 -0400

    Set up the views

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var twitter_config = require("./config");
 
 var app = express();
 
+app.set("view engine", "ejs");
+
 var client = new Twit({
   consumer_key: twitter_config.CONSUMER_KEY,
   consumer_secret: twitter_config.CONSUMER_SECRET,
@@ -12,6 +14,12 @@ var client = new Twit({
   timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
 });
 
+//======
+//ROUTES
+//======
+app.get("/", function(req, res){
+    res.render("index");
+});
 
 app.listen(process.env.port | 8080, process.env.IP, function(){
     console.log("Running in port 8080");
