ceef4cb515c4f82ba4a88a3555484f852a548a3e
Harmeet Bindra <harmeet@harmeets-mbp.attlocal.net>
2017-03-23 00:36:26 -0400

    set up config files

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 var express = require("express");
-var Twitter = require("twitter");
+var Twit = require("twit");
 var twitter_config = require("./config");
 
 var app = express();
-var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+
+var client = new Twit({
+  consumer_key: twitter_config.CONSUMER_KEY,
+  consumer_secret: twitter_config.CONSUMER_SECRET,
+  access_token: twitter_config.ACCESS_TOKEN_KEY,
+  access_token_secret: twitter_config.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60*1000  // optional HTTP request timeout to apply to all requests.
 });
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
 
 app.listen(process.env.port | 8080, process.env.IP, function(){
     console.log("Running in port 8080");
